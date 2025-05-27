88e234bd64fef66534004573e19d2dad8ba23723
omar <omarqais225@gmail.com>
2025-05-28 02:53:24 +0300

    created user schema

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async (request, response) => {
+    response.send("sign up page");
+};
+export const login = async (request, response) => {
+    response.send("login page");
+};
+export const logout = async (request, response) => {
+    response.send("logout page");
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose, { mongo } from 'mongoose';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
@@ -8,6 +10,9 @@ const MongoUrl = process.env.MONGOURL;
 app.get('/', (req, res) => {
     res.send('<h1>Hello world!!</h1>');
 });
+
+app.use('/api/auth', authRoutes);
+
 mongoose.connect(MongoUrl).then(() => {
     console.log('Database is connected successfully!!');
     app.listen(port, () => {
