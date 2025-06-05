b61379344ea474ea15738d69cffb299029439976
omar <omarqais225@gmail.com>
2025-06-05 19:03:13 +0300

    create request routes

---
+++ backend/src/routes/request.route.js
@@ -0,0 +1,7 @@
+import express from 'express';
+const router = express.Router();
+router.post('/add-request',);
+router.post('/approve-request',);
+router.post('/reject-request',);
+router.delete('/delete-request',);
+export default router;
