4ce33cdbaee9e0e50060e01096ca6a6feb0ccce8
omar <omarqais225@gmail.com>
2025-07-11 02:26:38 +0300

    fix the error

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,10 +7,8 @@ import requestRoutes from './routes/request.route.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import path from "path";
-import { fileURLToPath } from "url";
-dotenv.config();
-const __filename = fileURLToPath(process.env.CLIENT_URL);
 const __dirname = path.dirname(__filename);
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
@@ -21,10 +19,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/book', bookRoutes);
 app.use('/api/request', requestRoutes);
 if (process.env.NODE_ENV === "production") {
-    const frontendPath = path.resolve(__dirname, "../../frontend/dist");
-    app.use(express.static(frontendPath));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(frontendPath, "index.html"));
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
     });
 }
 mongoose.connect(MongoUrl).then(() => {
