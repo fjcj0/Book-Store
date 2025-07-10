06a60e342dd1ec26db7f31b89235bd5e5d200d75
omar <omarqais225@gmail.com>
2025-07-11 00:21:25 +0300

    try to fix error again

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,15 +17,11 @@ app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 const MongoUrl = process.env.MONGOURL;
 app.use(express.json());
 app.use(cookieParser());
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world!!</h1>');
-});
 
-/*
 app.use('/api/auth', authRoutes);
 app.use('/api/book', bookRoutes);
 app.use('/api/request', requestRoutes);
-*/
+
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
