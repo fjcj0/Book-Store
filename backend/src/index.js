579566d0803d6ae0152799a135d00291af34fa7f
omar <omarqais225@gmail.com>
2025-07-10 23:35:22 +0300

    fix an error again

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ app.use('/api/request', requestRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "backend/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
