08fa9ede07a70ebf2438141a00bdd070a6df016b
omar <omarqais225@gmail.com>
2025-07-11 02:22:45 +0300

    try again

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,9 @@ import requestRoutes from './routes/request.route.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import path from "path";
-const __dirname = path.resolve();
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
@@ -19,9 +21,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/book', bookRoutes);
 app.use('/api/request', requestRoutes);
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    const frontendPath = path.resolve(__dirname, "../../frontend/dist");
+    app.use(express.static(frontendPath));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(frontendPath, "index.html"));
     });
 }
 mongoose.connect(MongoUrl).then(() => {
